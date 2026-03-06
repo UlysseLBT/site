2f6cc5eb53a3dabc46dca8909454804acde10449
UlysseLBT <lebouteiller.ulysse@gmail.com>
2026-03-06 13:21:06 +0100

    feat:"refonte api"

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
+  env: {
+    ZOOM_ACCOUNT_ID: "LybRLNleRWePp5iK6jDS1A",
+    ZOOM_CLIENT_ID: "b0_NJ2ggRCi1vAw7U2GtMg",
+    ZOOM_CLIENT_SECRET: "0SHrvEroTR7YaunkucYDA03mbP8qEglV",
+  },
 };
 
 export default nextConfig;
